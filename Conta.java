2aa1f34ea034d2bccae900fd1ab314dca14eb4b8
MATHEUS AGUIAR MATOS <matheus1999aguiar@gmail.com>
2021-10-01 19:08:23 -0300

    Adicionei classes
    Adicionei as classes 'Conta' e 'Menu'

---
+++ Conta.java
@@ -0,0 +1,19 @@
+public class Conta {
+    
+  String nome;
+  long cpf;
+  int agencia;
+  int numConta;
+
+
+ public void saque(){
+
+ }
+
+  
+ public void deposito(){
+
+ }
+
+
+}
---
+++ Menu.java
@@ -0,0 +1,16 @@
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
+import java.util.Scanner;
+public class Menu {
+ public static void main(String[] args){
+
+     System.out.println("--- BANCO DIGITAL ---");
+     System.out.println("(1) -> ACESSAR CONTA");
+     System.out.println("(2) -> ABRIR CONTA");
+     
+
+
+ }
+
+    
+}
