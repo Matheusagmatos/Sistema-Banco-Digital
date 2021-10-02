2ace2f69177a85207dd48caca1b7b7bb6ebe263f
MATHEUS AGUIAR MATOS <matheus1999aguiar@gmail.com>
2021-10-01 21:14:22 -0300

    Implementação da classe 'Menu'

--- Menu.java
+++ Menu.java
@@ -1,14 +1,33 @@
 import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Menu {
  public static void main(String[] args){
+
+     Scanner input = new Scanner(System.in);
+
+     //cria uma lista para armazenar todas as contas criadas
+     ArrayList<Conta> listaContas = new ArrayList<>();
 
      System.out.println("--- BANCO DIGITAL ---");
      System.out.println("(1) -> ACESSAR CONTA");
      System.out.println("(2) -> ABRIR CONTA");
      
+     int option = input.nextInt(); 
 
+      if(option == 1){
+        System.out.println("INFORME O NÚMERO DA CONTA:");
+         int numDigitado = input.nextInt();
+        System.out.println("INFORME A SENHA DE 4 DÍGITOS");
+         int senhaDigitada = input.nextInt();
+
+          //verifica se a conta já existe percorrendo a 'listaContas'
+          for(int i = 0; i < listaContas.length; i++){
+              
+          }
+      }
 
  }
 
