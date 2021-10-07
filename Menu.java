cfa55df3b909023f51d506faf51c688f29defe8f
MATHEUS AGUIAR MATOS <matheus1999aguiar@gmail.com>
2021-10-07 18:51:45 -0300

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 import java.util.Scanner;
 public class Menu {
@@ -7,6 +8,9 @@ public class Menu {
 
      //cria uma lista para armazenar todas as contas criadas
      ArrayList<Conta> listaContas = new ArrayList<>();
+
+     //TESTE
+     System.out.println("Tamanho da listaContas: " + listaContas.size());
 
      System.out.println("--- BANCO DIGITAL ---");
      System.out.println("(1) -> ACESSAR CONTA");
