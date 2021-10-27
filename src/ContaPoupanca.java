fa7ed4993da77ad000db83d952b8ad5087722c43
MATHEUS AGUIAR MATOS <matheus1999aguiar@gmail.com>
2021-10-26 22:37:09 -0300

    Update ContaPoupanca.java
    Alteração no construtor da classe

--- src/ContaPoupanca.java
+++ src/ContaPoupanca.java
@@ -2,7 +2,7 @@ package src;
 
 public class ContaPoupanca extends Conta{
 
-    public ContaPoupanca(String nome, long cpf, String agencia, int numConta, int senhaConta, double saldo) {
+    public ContaPoupanca(String nome, String cpf, String agencia, int numConta, int senhaConta, double saldo) {
         super(nome, cpf, agencia, numConta, senhaConta, saldo);
     }
 
