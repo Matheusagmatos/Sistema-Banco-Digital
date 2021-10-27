36d299429e3e7e8520bb7314f15ae0625ac6825a
MATHEUS AGUIAR MATOS <matheus1999aguiar@gmail.com>
2021-10-27 17:51:34 -0300

    Update Validations.java

--- src/Validations.java
+++ src/Validations.java
@@ -11,6 +11,33 @@ public class Validations {
 
   public static boolean validarCPF(String cpfDigitado){
     System.out.println();
+
+    char[] cpfFormatado = cpfDigitado.toCharArray();
+
+    if(cpfFormatado[0] >= 0 && cpfFormatado[0] <= 9){
+      if(cpfFormatado[1] >= 0 && cpfFormatado[1] <= 9){
+        if(cpfFormatado[2] >= 0 && cpfFormatado[2] <= 9){
+          if(cpfFormatado[3] >= 0 && cpfFormatado[3] <= 9){
+            if(cpfFormatado[4] >= 0 && cpfFormatado[4] <= 9){
+              if(cpfFormatado[5] >= 0 && cpfFormatado[5] <= 9){
+                if(cpfFormatado[6] >= 0 && cpfFormatado[6] <= 9){
+                  if(cpfFormatado[7] >= 0 && cpfFormatado[7] <= 9){
+                    if(cpfFormatado[8] >= 0 && cpfFormatado[8] <= 9){
+                      if(cpfFormatado[9] >= 0 && cpfFormatado[9] <= 9){
+                        if(cpfFormatado[10] >= 0 && cpfFormatado[10] <= 9){
+                         return true;
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
     do{
      if(!(cpfDigitado.length() == 11)){
        System.out.println("Inválido!");
@@ -24,13 +51,53 @@ public class Validations {
 
 
 
-  public static boolean validarNumeroDaConta(ArrayList<Conta> listaContas, int numConta){
+  public static boolean validarNumeroDaContaGerado(ArrayList<Conta> listaContas, int numConta){
+    if(numConta == 1000 || numConta == 1111){
+      return false;
+    }if(numConta == 2000 || numConta == 2222){
+      return false;
+    }if(numConta == 3000 || numConta == 3333){
+      return false;
+    }if(numConta == 4000 || numConta == 4444){
+      return false;
+    }if(numConta == 5000 || numConta == 5555){
+      return false;
+    }if(numConta == 6000 || numConta == 6666){
+      return false;
+    }if(numConta == 7000 || numConta == 7777){
+      return false;
+    }if(numConta == 8000 || numConta == 8888){
+      return false;
+    }if(numConta == 9000 || numConta == 9999){
+      return false;
+    }
      for(int i = 0; i < listaContas.size(); i++){
        if(listaContas.get(i).getNumConta() == numConta){
         return false;
        }
      }
     return true;
+  }
+
+
+  
+  public static boolean validarContaeSenhaDeAcesso(ArrayList<Conta> listaContas, int numContaDigitado, int senhaContaDigitada){
+    boolean contaExiste = false;
+     for(int i = 0; i < listaContas.size(); i++){
+       if(listaContas.get(i).getNumConta() == numContaDigitado){
+         contaExiste = true;
+        if(listaContas.get(i).getSenhaConta() == senhaContaDigitada){
+          return true;
+        }
+        if(contaExiste == true){
+           System.out.println("Senha incorreta");
+           System.out.println("OPERAÇÃO ENCERRADA!");
+           System.exit(0);
+        }
+       }
+     }
+     System.out.println("Conta e/ou senha inválidos!");
+     return false;
   }
 
 
@@ -41,6 +108,7 @@ public class Validations {
     if(senhaDigitada < 1000 || senhaDigitada > 9999){
       System.out.println("Senha inválida!");
       System.out.println("Digite 4 dígitos numéricos");
+      System.out.println();
       return false;
     }
     //Verifica se a senha possui mais de 1 dígito igual
