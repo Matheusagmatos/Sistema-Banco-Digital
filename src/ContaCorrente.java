beac9042e3b0f4af3377c39ef26477dce57a93ef
MATHEUS AGUIAR MATOS <matheus1999aguiar@gmail.com>
2021-10-26 22:36:54 -0300

    Update ContaCorrente.java
    Alteração no construtor da classe

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -2,7 +2,7 @@ package src;
 
 public class ContaCorrente extends Conta{
 
-    public ContaCorrente(String nome, long cpf, String agencia, int numConta, int senhaConta, double saldo) {
+    public ContaCorrente(String nome, String cpf, String agencia, int numConta, int senhaConta, double saldo) {
         super(nome, cpf, agencia, numConta, senhaConta, saldo);
     }
 
