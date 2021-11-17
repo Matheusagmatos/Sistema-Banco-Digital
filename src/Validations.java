14972c62f2b667391abcb0962a440c3ea01a70e2
MATHEUS AGUIAR MATOS <matheus1999aguiar@gmail.com>
2021-11-16 23:18:53 -0300

    Update Validations.java

--- src/Validations.java
+++ src/Validations.java
@@ -8,7 +8,17 @@ public class Validations {
   static Scanner input = new Scanner(System.in);
 
 
-
+  /**
+   * Método para validação do número de CPF digitado pelo usuário.
+   * <p>
+   * Percorre cada um dos 11 caracteres digitados pelo usuário, e verifica
+   * se estes caracteres são algarismos que estão entre 0 e 9 ou se foi digitado
+   * algum tipo de dado que não seja um número. 
+   * 
+   * @param cpfDigitado o número de CPF digitado pelo usuário
+   * 
+   * @return 'true' se o CPF for válido, ou 'false' se o cpf for inválido
+   */
   public static boolean validarCPF(String cpfDigitado){
     System.out.println();
 
@@ -50,7 +60,21 @@ public class Validations {
 
 
 
-
+  /**
+   * Método para validação do número da conta gerado pelo algoritmo.
+   * <p>
+   * O algoritmo gera um número aleatório de quatro dígitos, através da classe
+   * 'Random' e faz a chamada para este método que vai verificar se a sequência gerada,
+   * ou seja, se o número de conta gerado já existe e se é uma sequência válida.
+   * <p>
+   * Obs: não serão válidos valores com três zeros (000) ao final e nem sequências com
+   * quatro números iguais (exe: 1111, 2222);
+   * 
+   * @param listaContas uma lista do tipo 'Conta', que armazena todas as contas ativas no banco digital
+   * @param numConta o número da conta (sequência de 4 dígitos) que foi gerado pelo algoritmo
+   * 
+   * @return 'true' se o número gerado for válido, ou 'false' se o número gerado for inválido
+   */
   public static boolean validarNumeroDaContaGerado(ArrayList<Conta> listaContas, int numConta){
     if(numConta == 1000 || numConta == 1111){
       return false;
@@ -80,7 +104,20 @@ public class Validations {
   }
 
 
-  
+  /**
+   * Método que valida o acesso do usuário à conta bancária.
+   * <p>
+   * Verifica se o número da conta e senha digitados pelo usuário são válidos.
+   * <p>
+   * Caso os dados informamdos sejam válidos, o algoritmo libera o acesso à conta
+   * do usuário.
+   * 
+   * @param listaContas uma lista do tipo 'Conta', que armazena todas as contas ativas no banco digital
+   * @param numContaDigitado o número da conta (sequência de 4 dígitos) que foi digitado pelo usuário
+   * @param senhaContaDigitada a senha (de 4 dígitos) digitada pelo usuário
+   * 
+   * @return 'true' se os dados informamdos forem válidos, ou 'false' se os dados informados forem inválidos
+   */
   public static boolean validarContaeSenhaDeAcesso(ArrayList<Conta> listaContas, int numContaDigitado, int senhaContaDigitada){
     boolean contaExiste = false;
      for(int i = 0; i < listaContas.size(); i++){
@@ -101,7 +138,16 @@ public class Validations {
   }
 
 
-
+  /**
+   * Método que valida a senha digitada pelo usuário.
+   * <p>
+   * Válido tanto para a senha cadastrada no momento da abertura da conta, quanto
+   * para a senha digitada no momento de acesso à conta.
+   * 
+   * @param senhaDigitada a senha (de 4 dígitos) digitada pelo usuário
+   * 
+   * @return 'true' se a senha for válida, ou 'false' se for inválida
+   */
   public static boolean validarSenha(int senhaDigitada){
     System.out.println();
     //Verifica se a senha possui menos de 4 dígitos
